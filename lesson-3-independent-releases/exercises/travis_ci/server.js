5e5f7fe0054967586bc693db7224dcb28f959463
Lawal Hakeem <lawizyhal@gmail.com>
2021-11-14 11:08:09 +0100

    feat: console-changes

--- lesson-3-independent-releases/exercises/travis_ci/server.js
+++ lesson-3-independent-releases/exercises/travis_ci/server.js
@@ -4,7 +4,7 @@ function sleep(ms) {
 
 async function main() {
   while(true) {
-    console.log('Laweezy Containers rule!');
+    console.log('Hakeems Containers rule!');
     await sleep(5000);
   }
 }
